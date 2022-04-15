9f73524a81b0ffc3163c5e78774c083f4d433f47
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-04-15 18:16:59 +0300

    Done

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "shell.h"
 
 int client()
@@ -9,35 +8,11 @@ int client()
     {
         printf("error in connectiong...\n");
         return -1;
-=======
-#include <unistd.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h> 
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define BUFF_SIZE 1024
-
-char *IP = "localhost";
-int PORT = 5500;
-struct sockaddr_in add;
-socklen_t addr_size;
-char buff[BUFF_SIZE];
-
-int main(){
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock < 0){
-        printf("error in connectiong...\n");
-        exit(1);
->>>>>>> cc8f9266960b2c56f15b78fdc47d9630e21667e6
     }
     printf("success in creating socket\n");
 
     // do we need memset??
     add.sin_family = AF_INET;
-<<<<<<< HEAD
     add.sin_port = 3000;
     add.sin_addr.s_addr = INADDR_ANY;
 
@@ -50,15 +25,4 @@ int main(){
     printf("connected to server on port: %d", add.sin_port);
     
     return 1;
-=======
-    add.sin_port = htons(9000);
-    add.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    connect(sock, (struct sock_addr*)&add, sizeof(add));
-    printf("connected to server on port: %d", PORT);
-    // dup2(1,123);
-    // dup2(sock,1);
-
-    return 0;
->>>>>>> cc8f9266960b2c56f15b78fdc47d9630e21667e6
 }
--- server.c
+++ server.c
@@ -1,15 +1,10 @@
 #include <unistd.h>
 #include <netinet/in.h>
-<<<<<<< HEAD
 #include <netinet/tcp.h>
-=======
-#include <netinet/tcp.h> 
->>>>>>> cc8f9266960b2c56f15b78fdc47d9630e21667e6
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-<<<<<<< HEAD
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -45,33 +40,10 @@ int main()
         printf("error in connecting...\n");
         exit(1);
     }
-=======
-
-#define BUFF_SIZE 1024
-
-char *IP = "localhost";
-int PORT = 5500;
-int sock_server;
-int sock_client;
-
-char buff[BUFF_SIZE];
-
-struct sockaddr_in server_addr, client_addr;
-socklen_t addr_size;
-
-int main(){
-    sock_server = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock_server < 0){
-        printf("error in connecting...\n");
-        exit(1);
-    }
-
->>>>>>> cc8f9266960b2c56f15b78fdc47d9630e21667e6
     printf("succsessfully created TCP socket\n");
 
     // do we need memset??
     server_addr.sin_family = AF_INET;
-<<<<<<< HEAD
     server_addr.sin_port = 3000;
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
@@ -98,35 +70,10 @@ int main(){
         bzero(buff, BUFF_SIZE);
         if (!recv(sock_client, buff, sizeof(buff), 0))
         {
-=======
-    server_addr.sin_port = htons(9000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    int temp = bind(sock_server, (struct sock_addr*)&server_addr, sizeof(server_addr));
-    if(temp < 0){
-        printf("error in bind\n");
-        exit(1);
-    }
-    printf("bind successfully to port number: %d", PORT);
-
-    listen(sock_server,1);
-    printf("listening...\n");
-    int flag = 1;
-    while(1){
-        if(flag){
-        addr_size = sizeof(client_addr);
-        sock_client = accept(sock_server, (struct sock_addr*)&client_addr, &addr_size);
-        flag = 0;
-        printf("client is connected on port number: %d", PORT);
-        }
-        bzero(buff, BUFF_SIZE);
-        if(!recv(sock_client, buff, sizeof(buff), 0)){
->>>>>>> cc8f9266960b2c56f15b78fdc47d9630e21667e6
             close(sock_client);
             // return the connection
             flag = 1;
         }
-<<<<<<< HEAD
         else
         {
             for (int i = 0; i < BUFF_SIZE; i++)
@@ -134,17 +81,7 @@ int main(){
                 printf("%c", buff[i]);
             }
             puts("");
-=======
-        else{
-            for(int i = 0 ; i < BUFF_SIZE ; i++){
-                printf("%c", buff[i]);
-            }
->>>>>>> cc8f9266960b2c56f15b78fdc47d9630e21667e6
         }
     }
     return 0;
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> cc8f9266960b2c56f15b78fdc47d9630e21667e6
